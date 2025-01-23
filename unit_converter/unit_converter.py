330d92baa078c920980383958206529fbc947ecb
NachoSAG <ignacioborlenghi@gmail.com>
2025-01-22 22:39:38 -0300

    Initial commit

--- unit_converter/unit_converter.py
+++ unit_converter/unit_converter.py
@@ -7,7 +7,13 @@ def index() -> rx.Component:
     return rx.container(
             rx.vstack(
             rx.heading("Unit Converter"),
-            rx.hstack(
+            tabs(),
+            align="center"
+        )
+    )
+
+def tabs():
+    return rx.hstack(
                 rx.tabs.root(
                     rx.tabs.list(
                         rx.tabs.trigger("Lenght", value="lenght_tab"),
@@ -18,51 +24,54 @@ def index() -> rx.Component:
                         rx.cond(
                             FormState.show_results,
                             # Results screen
-                            rx.vstack(
-                                rx.heading("Results"),
-                                rx.text(FormState.form_data.to_string()),
-                                rx.button(
-                                    "Reset",
-                                    on_click=FormState.reset_screen,
-                                ),
-                            ),
+                            results_screen(),
                             # Form screen 
-                            rx.vstack(
-                                rx.text("Enter the lenght to convert"),
-                                rx.form(
-                                    rx.vstack(
-                                        rx.input(name="input"),
-                                        rx.text("Unit to convert from"),
-                                        rx.select(
-                                        Units.LENGHT.value,
-                                        value=SelectState.from_unit,
-                                        on_change=SelectState.convert_from,
-                                        name="convert_from"
-                                        ),
-                                        rx.text("Unit to convert to"),
-                                        rx.select(
-                                            Units.LENGHT.value,
-                                            value=SelectState.to_unit,
-                                            on_change=SelectState.convert_to,
-                                            name="convert_to"
-                                        ),
-                                        rx.button(
-                                            "Submit",
-                                            type="submit"
-                                        )
-                                    ),
-                                    on_submit=FormState.handle_submit,
-                                    reset_on_submit=True
-                                ),
-                            ),
+                            form_screen(),
                         ),
                         value="lenght_tab"
                     )
                 )
-            ),
-            align="center"
-        )
-    )
+            )
+
+def results_screen():
+    return rx.vstack(
+                    rx.heading("Results"),
+                    rx.text(FormState.form_data.to_string()),
+                    rx.button(
+                        "Reset",
+                        on_click=FormState.reset_screen,
+                    ),
+                )
+
+def form_screen():
+    return rx.vstack(
+                    rx.text("Enter the lenght to convert"),
+                    rx.form(
+                        rx.vstack(
+                            rx.input(name="input"),
+                            rx.text("Unit to convert from"),
+                            rx.select(
+                            Units.LENGHT.value,
+                            value=SelectState.from_unit,
+                            on_change=SelectState.convert_from,
+                            name="convert_from"
+                            ),
+                            rx.text("Unit to convert to"),
+                            rx.select(
+                                Units.LENGHT.value,
+                                value=SelectState.to_unit,
+                                on_change=SelectState.convert_to,
+                                name="convert_to"
+                            ),
+                            rx.button(
+                                "Submit",
+                                type="submit"
+                            )
+                        ),
+                        on_submit=FormState.handle_submit,
+                        reset_on_submit=True
+                    ),
+                )
 
 app = rx.App()
 app.add_page(index)
